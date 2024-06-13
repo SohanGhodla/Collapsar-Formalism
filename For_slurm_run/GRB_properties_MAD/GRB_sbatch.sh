ca8dd9c80c61dac6ebf742f437cdf30882b33d92
Sohan <sgho069@aucklanduni.ac.nz>
2024-06-13 17:04:38 +1200

    removed slurm run dir

--- For_slurm_run/GRB_properties_MAD/GRB_sbatch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --job-name=PytonMultiprocessing
-#SBATCH --cpus-per-task=12  # Number of logical CPUs
-#SBATCH --time=6:00:00
-#SBATCH --mem-per-cpu=3GB # Memory per logical CPU
-#SBATCH -o ./Slurm_output/%A_%a.out # STDOUT
-
-module load Python/3.7.3-gimkl-2018b
-python run_collapsar.py
--- For_slurm_run/GRB_properties_nonrel_NTD/README.txt
+++
@@ -1,5 +0,0 @@
-execute the run_GRB.py file to sbatch the job to slurm.
-You would need to modify the GRB_sbatch.sh file to match your needs
-
-The only other revelant files are 1) collapsar.py 2) run_collapsar.py 
-The rest are there as part of MESA reader dependencies.
